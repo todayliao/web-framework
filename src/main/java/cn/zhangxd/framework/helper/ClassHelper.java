5e49eb49a59836c1d6202d4491c412fd34bd173e
zhangxd1989 <zhangxd_2011sz@163.com>
2017-02-26 23:55:43 +0800

    增加类解析器

---
+++ src/main/java/cn/zhangxd/framework/helper/ClassHelper.java
@@ -0,0 +1,69 @@
+package cn.zhangxd.framework.helper;
+
+import cn.zhangxd.framework.annotation.Controller;
+import cn.zhangxd.framework.annotation.Service;
+import cn.zhangxd.framework.util.ClassUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 类操作助手类
+ *
+ * @author zhangxd
+ */
+public class ClassHelper {
+
+    /**
+     * 定义类集合（用于存放所记载的类）
+     */
+    private static final Set<Class<?>> CLASS_SET;
+
+    static {
+        String basePackage = ConfigHelper.getAppBasePackage();
+        CLASS_SET = ClassUtil.getClassSet(basePackage);
+    }
+
+    /**
+     * 获取应用包名下所有类
+     */
+    public static Set<Class<?>> getClassSet() {
+        return CLASS_SET;
+    }
+
+    /**
+     * 获取包名下的所有service类
+     */
+    public static Set<Class<?>> getServiceClassSet() {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Service.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 获取包名下的所有controller类
+     */
+    public static Set<Class<?>> getControllerClassSet() {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Controller.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 获取应用包名下所有Bean类（包括：Service、Controller等）
+     */
+    public static Set<Class<?>> getBeanClassSet() {
+        Set<Class<?>> beanClassSet = new HashSet<Class<?>>();
+        beanClassSet.addAll(getServiceClassSet());
+        beanClassSet.addAll(getControllerClassSet());
+        return beanClassSet;
+    }
+}
